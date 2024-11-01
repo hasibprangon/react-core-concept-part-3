b6d94e6626e15dca99fb6605f036cea4e7e3ea98
hasibprangon <hasibulhossainp09@gmail.com>
2024-11-02 02:28:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+     <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
